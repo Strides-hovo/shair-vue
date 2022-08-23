75b2412962cce652e8c101a78e22ed01057d5b5d
Strides-hovo <hovik_ghambaryan@mail.ru>
2022-08-23 07:23:34 +0300

    articles, article, about, translate pages

--- database/seeders/PageSeeder.php
+++ database/seeders/PageSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Page;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\PageTranslate;
 
 class PageSeeder extends Seeder
 {
@@ -15,6 +16,39 @@ class PageSeeder extends Seeder
      */
     public function run()
     {
-        Page::factory(1)->create();
+        $faker = \Faker\Factory::create('ru_RU');
+        $dates = [
+            [
+                'name' => 'product',
+                'sub_menu' => false,
+            ],
+            [
+                'name' => 'article',
+                'sub_menu' => true,
+            ],
+        ];
+        
+            Page::insert($dates);
+        
+
+
+        $translates = [
+            [
+                'page_id' => 1,
+                'title' => $faker->title,
+                'slug' => $faker->slug,
+                'meta_title' => $faker->name,
+                'language_id' => 1,
+            ],
+            [
+                'page_id' => 2,
+                'title' => $faker->title,
+                'slug' => $faker->slug,
+                'meta_title' => $faker->name,
+                'language_id' => 1,
+            ],
+        ];
+        
+        PageTranslate::insert($translates);
     }
 }
